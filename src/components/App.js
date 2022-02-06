3ac8c03ec88bb1accd4a20afae3643f11449b766
Jack Pena <penajackc@gmail.com>
2022-02-06 15:55:22 -0700

    commit tree c41cdce6f0b0ef50b7263f9d18e4a6c616383fef

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+	const [dogPic, setDogPic] = useState(false);
+
+	useEffect(
+		() =>
+			fetch("https://dog.ceo/api/breeds/image/random")
+				.then((res) => res.json())
+				.then((data) => {
+                    setDogPic(data.message)
+                }),
+		[]
+	);
+
+	if (!dogPic) {
+		return <p>Loading...</p>;
+	}
+	return (
+		<div>
+			<img src={dogPic} alt="A Random Dog" />
+		</div>
+	);
+}
+
+export default App;
